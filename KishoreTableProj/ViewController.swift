325a067d01823426136d8328cbb4cf4cb31e27e7
kishorekris <kishorekris@vmware.com>
2020-04-12 18:18:54 +0530

    Kishore - Sample Table views

--- KishoreTableProj/ViewController.swift
+++ KishoreTableProj/ViewController.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+class ViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+    
+    var dataModel = ["Mango", "Apple" , "Grapes" , "Guava", "Orange"]
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.dataModel.count
     }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "protoCell")!
+        let dataModelrow = self.dataModel[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = dataModelrow
+        cell.imageView?.image = UIImage(named: "Kishore")
+        return cell
+    }
+    
+    // row deletion
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            dataModel.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+    
+    //setting table header
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "Sample Table View"
+    }
+    
 
 
 }
